e3e75db824c0ba976f9f6ec6e50739f0d373cea8
Fidel Adar <adarfidel.ogw@gmail.com>
2025-11-15 19:30:35 +0300

    5. Implement the User Modal

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -5,13 +5,6 @@ export interface PostProps {
   body: string;
 }
 
-export interface PostProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
-
 export interface UserProps {
   id: number;
   name: string;
